a34a85236a55ef0c7ebc6fb10dfabfe8d4ea7ad2
Jayesh Panchal <jspanchal@ymail.com>
2023-09-19 08:58:30 +0530

    Add isString

---
+++ src/query/is-string.js
@@ -0,0 +1,10 @@
+/**
+ * Return whether `value` is of type `string`
+ * @param value
+ * @returns {boolean}
+ */
+const isString = (value) => {
+  return typeof value === 'string';
+};
+
+export default isString;
